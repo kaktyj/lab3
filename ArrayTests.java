54908084bc16dfc022a6fed4408a038fe6093d46
kaktyj <115811254+kaktyj@users.noreply.github.com>
2023-10-25 15:01:36 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2(){
+    int[] input = {1,2,3};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{3,2,1}, input );
+    
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2,3 };
+    assertArrayEquals(new int[]{ 3,2,1}, ArrayExamples.reversed(input1));
+  }
 }
